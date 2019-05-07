4f38605b9d59d3c0a3ac46a7c975b2b372d503e4
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-07 18:56:05 +0500

    add movie info mocks

--- src/mocks/movies.js
+++ src/mocks/movies.js
@@ -1,1 +1,27 @@
-export const MOVIES = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Baby Driver`];
+export const MOVIES = [
+  {
+    id: `fantastic-beasts-the-crimes-of-grindelwald`,
+    img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  },
+  {
+    id: `bohemian-rhapsody`,
+    img: `img/bohemian-rhapsody.jpg`,
+    title: `Bohemian Rhapsody`,
+  },
+  {
+    id: `seven-years-in-tibet`,
+    img: `img/seven-years-in-tibet.jpg`,
+    title: `Seven Years in Tibet`,
+  },
+  {
+    id: `what-we-do-in-the-shadows`,
+    img: `img/what-we-do-in-the-shadows.jpg`,
+    title: `What We Do in the Shadows`,
+  },
+  {
+    id: `pulp-fiction`,
+    img: `img/pulp-fiction.jpg`,
+    title: `Pulp Fiction`,
+  },
+];
