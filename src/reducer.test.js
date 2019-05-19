00b96ebbd5bf24ba5243f7504591c9e47d6d7ea7
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-20 01:17:49 +0500

    add reducer tests

---
+++ src/reducer.test.js
@@ -0,0 +1,76 @@
+import {
+  ActionCreator,
+  reducer,
+} from "reducer";
+
+describe(`Action creators work correctly`, () => {
+  it(`Action creator for genre change returns correct action`, () => {
+    expect(ActionCreator.changeGenre(`comedies`)).toEqual({
+      type: `CHANGE_GENRE`,
+      payload: `comedies`,
+    });
+  });
+
+  it(`Action creator for genre based movie filtering returns correct movie array when genre is chosen`, () => {
+    expect(ActionCreator.getMovies(
+        `comedies`,
+        [{
+          id: 1,
+          img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+          title: `Fantastic Beasts: The Crimes of Grindelwald`,
+          preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+          genre: `comedies`,
+        },
+        {
+          id: 2,
+          img: `img/bohemian-rhapsody.jpg`,
+          title: `Bohemian Rhapsody`,
+          preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+          genre: `crime`,
+        }])).toEqual({
+      type: `GET_MOVIES`,
+      payload: [{
+        id: 1,
+        img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+        title: `Fantastic Beasts: The Crimes of Grindelwald`,
+        preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+        genre: `comedies`,
+      }],
+    });
+  });
+
+  it(`Action creator for genre based movie filtering returns array of all movies when genre is defaulted to all genres`, () => {
+    expect(ActionCreator.getMovies(
+        `all genres`,
+        [{
+          id: 1,
+          img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+          title: `Fantastic Beasts: The Crimes of Grindelwald`,
+          preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+          genre: `comedies`,
+        },
+        {
+          id: 2,
+          img: `img/bohemian-rhapsody.jpg`,
+          title: `Bohemian Rhapsody`,
+          preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+          genre: `crime`,
+        }])).toEqual({
+      type: `ALL_MOVIES`,
+      payload: [{
+        id: 1,
+        img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+        title: `Fantastic Beasts: The Crimes of Grindelwald`,
+        preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+        genre: `comedies`,
+      },
+      {
+        id: 2,
+        img: `img/bohemian-rhapsody.jpg`,
+        title: `Bohemian Rhapsody`,
+        preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+        genre: `crime`,
+      }],
+    });
+  });
+});
