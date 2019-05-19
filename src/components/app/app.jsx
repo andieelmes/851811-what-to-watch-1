bcbe119bad4cbc5cd96a7585dd507aee85966a90
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-20 00:53:49 +0500

    connect app to store

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
 import Main from 'components/main/main.jsx';
+import {ActionCreator} from "reducer";
 
-import {MOVIES} from 'mocks/movies';
+const propTypes = {
+  movies: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        img: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+      })
+  ),
+  onGenreClick: PropTypes.func.isRequired,
+  genre: PropTypes.string,
+};
 
-const App = () => (
-  <Main
-    movies={MOVIES}
-  />
-);
+const defaultProps = {
+  genre: `all genres`,
+};
 
-export default App;
+const App = (props) => {
+  const {
+    genre,
+    movies,
+    onGenreClick,
+  } = props;
+
+
+  return (
+    <Main
+      genre={genre}
+      movies={movies}
+      onGenreClick={(clickedGenre) => onGenreClick(clickedGenre, movies)}
+    />
+  );
+};
+
+const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
+  movies: state.movies,
+  genre: state.genre,
+});
+
+
+const mapDispatchToProps = (dispatch) => ({
+  onGenreClick: (genre, movies) => {
+    dispatch(ActionCreator.changeGenre(genre));
+    dispatch(ActionCreator.getMovies(genre, movies));
+  },
+});
+
+App.propTypes = propTypes;
+App.defaultProps = defaultProps;
+
+export {App};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
