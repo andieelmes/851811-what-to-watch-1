9f938789d78ac2dbe568a4643044f630c39ac66f
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-28 15:01:35 +0500

    use selectors

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import Main from 'components/main/main.jsx';
-import {ActionCreator} from "reducer";
+import {ActionCreator} from "reducer/data/data";
+import {getGenres, getMovies} from "reducer/data/selectors";
 
 const propTypes = {
   movies: PropTypes.arrayOf(
@@ -14,10 +15,12 @@ const propTypes = {
   ),
   onGenreClick: PropTypes.func.isRequired,
   genre: PropTypes.string,
+  genres: PropTypes.arrayOf(PropTypes.string)
 };
 
 const App = (props) => {
   const {
+    genres,
     movies,
     onGenreClick,
   } = props;
@@ -25,21 +28,22 @@ const App = (props) => {
 
   return (
     <Main
+      genres={genres}
       movies={movies}
-      onGenreClick={(clickedGenre) => onGenreClick(clickedGenre, movies)}
+      onGenreClick={(clickedGenre) => onGenreClick(clickedGenre)}
     />
   );
 };
 
 const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
-  movies: state.movies,
+  movies: getMovies(state),
+  genres: getGenres(state)
 });
 
 
 const mapDispatchToProps = (dispatch) => ({
-  onGenreClick: (genre, movies) => {
+  onGenreClick: (genre) => {
     dispatch(ActionCreator.changeGenre(genre));
-    dispatch(ActionCreator.getMovies(genre, movies));
   },
 });
 
