d8de43d7a11d692c69fc6ea688b2e50ff5b2969d
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-29 02:21:29 +0500

    add sign in component

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import {Operation} from "reducer/user/user";
+
+const propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+class SignIn extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: ``,
+      password: ``,
+    };
+
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onEmailChange({target: {value}}) {
+    this.setState({
+      email: value
+    });
+  }
+
+  onPasswordChange({target: {value}}) {
+    this.setState({
+      password: value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const {
+      onSubmit,
+      history
+    } = this.props;
+    onSubmit({email: this.state.email, password: this.state.password});
+    history.push(`/`);
+  }
+
+  render() {
+    const {
+      email,
+      password,
+    } = this.state;
+
+    return (
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </header>
+
+        <div className="sign-in user-page__content">
+          <form action="#" className="sign-in__form" onSubmit={this.onSubmit}>
+            <div className="sign-in__fields">
+              <div className="sign-in__field">
+                <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" value={email} onChange={this.onEmailChange}/>
+                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+              </div>
+              <div className="sign-in__field">
+                <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" value={password} onChange={this.onPasswordChange}/>
+                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+              </div>
+            </div>
+            <div className="sign-in__submit">
+              <button className="sign-in__btn" type="submit">Sign in</button>
+            </div>
+          </form>
+        </div>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+SignIn.propTypes = propTypes;
+
+export {SignIn};
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: ({email, password}) => {
+    dispatch(Operation.login(email, password));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
