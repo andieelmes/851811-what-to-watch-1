a4ac2fa64f67346e19c9de7df4c6c0bbda212d3a
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-29 02:21:13 +0500

    add auth selectors and actions

--- src/reducer/user/selectors.js
+++ src/reducer/user/selectors.js
@@ -4,5 +4,9 @@ const NAMESPACE = Namespace.USER;
 
 
 export const getAuthorizationStatus = (state) => {
-  return state[NAMESPACE].isAuthorizationRequired;
+  return !state[NAMESPACE].isAuthorizationRequired;
+};
+
+export const getAvatar = (state) => {
+  return state[NAMESPACE].avatar;
 };
--- src/reducer/user/user.js
+++ src/reducer/user/user.js
@@ -1,12 +1,14 @@
-const initialState = {
-  isAuthorizationRequired: false,
-};
+import {ENDPOINT_URL} from 'server-variables';
 
+const initialState = {
+  isAuthorizationRequired: true,
+  avatar: ``,
+};
 
 const ActionType = {
   REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+  GET_USER_DATA: `GET_USER_DATA`
 };
-
 
 const ActionCreator = {
   requireAuthorization: (status) => {
@@ -15,12 +17,34 @@ const ActionCreator = {
       payload: status,
     };
   },
+
+  getUserData: (data) => {
+    return {
+      type: ActionType.GET_USER_DATA,
+      payload: data,
+    };
+  },
+};
+
+const Operation = {
+  login: (email, password) => (dispatch, _getState, api) => {
+    console.log(email, password);
+    return api.post(`/login`, {email, password})
+      .then((response) => {
+        const data = {
+          avatar: `${ENDPOINT_URL}${response.data.avatar_url.replace(`/wtw`, ``)}`,
+        }
+        dispatch(ActionCreator.requireAuthorization(false));
+        dispatch(ActionCreator.getUserData(data));
+      });
+  },
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.REQUIRED_AUTHORIZATION:
       return {...state, isAuthorizationRequired: action.payload}
+    case ActionType.GET_USER_DATA: return {...state, ...action.payload}
   }
 
   return state;
@@ -30,5 +54,6 @@ const reducer = (state = initialState, action) => {
 export {
   ActionCreator,
   ActionType,
+  Operation,
   reducer,
 };
