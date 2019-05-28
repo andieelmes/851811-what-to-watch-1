4c4e2cb5a458d6ed484a523b3a839ed2dfcf624c
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-29 02:31:15 +0500

    update tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
 import {App} from 'components/app/app.jsx';
 
 import {MOVIES, GENRES} from 'mocks/movies';
 
 describe(`App component`, () => {
-  it(`should render correctly`, () => {
+  it(`should render correctly when authorized`, () => {
     const tree = renderer
-      .create(<App
-        movies={MOVIES}
-        genres={GENRES}
-        onGenreClick={() => {}}
-      />, {createNodeMock: (el) => el})
+      .create(<BrowserRouter>
+        <App
+          movies={MOVIES}
+          genres={GENRES}
+          onGenreClick={() => {}}
+          user={{
+            authorized: true,
+            avatar: ``,
+          }}
+        />
+      </BrowserRouter>, {createNodeMock: (el) => el})
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`should render correctly for guests`, () => {
+    const tree = renderer
+      .create(<BrowserRouter>
+        <App
+          movies={MOVIES}
+          genres={GENRES}
+          onGenreClick={() => {}}
+          user={{
+            authorized: false,
+            avatar: ``,
+          }}
+        />
+      </BrowserRouter>, {createNodeMock: (el) => el})
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
 import Main from 'components/main/main.jsx';
 
 import {MOVIES, GENRES} from 'mocks/movies';
 
 describe(`Main component`, () => {
-  it(`should render correctly`, () => {
+  it(`should render correctly when authorized`, () => {
     const tree = renderer
-      .create(
-          <Main
-            movies={MOVIES}
-            genres={GENRES}
-            onGenreClick={() => {}}
-          />, {createNodeMock: (el) => el})
+      .create(<BrowserRouter>
+        <Main
+          movies={MOVIES}
+          genres={GENRES}
+          onGenreClick={() => {}}
+          user={{
+            authorized: true,
+            avatar: ``,
+          }}
+        />
+      </BrowserRouter>, {createNodeMock: (el) => el})
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`should render correctly for guests`, () => {
+    const tree = renderer
+      .create(<BrowserRouter>
+        <Main
+          movies={MOVIES}
+          genres={GENRES}
+          onGenreClick={() => {}}
+          user={{
+            authorized: false,
+            avatar: ``,
+          }}
+        />
+      </BrowserRouter>, {createNodeMock: (el) => el})
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
