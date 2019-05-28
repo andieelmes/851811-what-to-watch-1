73242f3daa7b548c29d63971b0a57a142d1ab271
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-29 02:52:51 +0500

    remove init func because of double rendering

--- src/index.js
+++ src/index.js
@@ -10,26 +10,21 @@ import reducer from "./reducer";
 import {Operation} from "reducer/data/data";
 import {createAPI} from './api';
 
-const init = () => {
-  const api = createAPI((...args) => store.dispatch(...args));
+const api = createAPI((...args) => store.dispatch(...args));
 
-  const store = createStore(
-      reducer,
-      compose(
-          applyMiddleware(thunk.withExtraArgument(api)),
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
-  );
+const store = createStore(
+    reducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
-  store.dispatch(Operation.loadMovies());
+store.dispatch(Operation.loadMovies());
 
-  ReactDOM.render(
-      <Provider store={store}>
-        <App/>
-      </Provider>,
-      document.getElementById(`root`)
-  );
-
-};
-
-init();
+ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.getElementById(`root`)
+);
