2dd6cbaffe7a4261f3747903866f04fb3397fd4c
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-06-02 16:58:18 +0500

    add favorites component

---
+++ src/components/favorites/favorites.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Favorites from 'components/favorites/favorites.jsx';
+
+describe(`Favorites component`, () => {
+  it(`should render correctly`, () => {
+    const tree = renderer
+      .create(<Favorites/>, {createNodeMock: (el) => el})
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
