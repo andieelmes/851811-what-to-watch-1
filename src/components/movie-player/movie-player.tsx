010c999c374bd1552a38a881ee5a58e362992b36
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-06-19 13:03:33 +0500

    add scrubbing, fix time left

--- src/components/movie-player/movie-player.tsx
+++ src/components/movie-player/movie-player.tsx
@@ -14,15 +14,18 @@ interface State {
   progress: number,
   timeLeft: number,
   height: number,
+  isMousedown: boolean,
 };
 
 class MoviePlayer extends React.PureComponent<Props, State> {
   _videoRef: React.RefObject<HTMLVideoElement>;
+  _progressRef: React.RefObject<HTMLProgressElement>;
 
   constructor(props) {
     super(props);
 
     this._videoRef = React.createRef();
+    this._progressRef = React.createRef();
 
     this.state = {
       isLoading: true,
@@ -31,11 +34,15 @@ class MoviePlayer extends React.PureComponent<Props, State> {
       progress: 0,
       timeLeft: 0,
       height: 0,
+      isMousedown: false,
     };
 
     this._togglePlay = this._togglePlay.bind(this);
     this._setFullscreen = this._setFullscreen.bind(this);
     this._onUpdate = this._onUpdate.bind(this);
+    this._onScrub = this._onScrub.bind(this);
+    this._onMouseDown = this._onMouseDown.bind(this);
+    this._onMouseUp = this._onMouseUp.bind(this);
   }
 
   componentDidMount() {
@@ -71,7 +78,8 @@ class MoviePlayer extends React.PureComponent<Props, State> {
       timeLeft,
       isPlaying,
       height,
-      isFullscreen
+      isFullscreen,
+      isMousedown
     } = this.state;
 
     const formattedTimeLeft = MoviePlayer.getFormattedTime(timeLeft)
@@ -99,7 +107,16 @@ class MoviePlayer extends React.PureComponent<Props, State> {
         <div className="player__controls">
           <div className="player__controls-row">
             <div className="player__time">
-              <progress className="player__progress" value={progress} max="100"></progress>
+              <progress
+                className="player__progress"
+                value={progress}
+                max="100"
+                ref={this._progressRef}
+                onClick={this._onScrub}
+                onMouseMove={(e) => isMousedown && this._onScrub(e)}
+                onMouseDown={this._onMouseDown}
+                onMouseUp={this._onMouseUp}
+              ></progress>
               <div className="player__toggler" style={{left: `${progress}%`}}>Toggler</div>
             </div>
             <div className="player__time-value">{formattedTimeLeft ? formattedTimeLeft : ''}</div>
@@ -159,6 +176,30 @@ class MoviePlayer extends React.PureComponent<Props, State> {
     })
   }
 
+  _onScrub(e) {
+    const video = this._videoRef.current;
+    const progressBar = this._progressRef.current;
+
+    const scrubTime = (e.nativeEvent.offsetX / progressBar.offsetWidth) * video.duration;
+
+    if (!scrubTime) return
+
+    video.currentTime = scrubTime;
+    this._onUpdate()
+  }
+
+  _onMouseDown() {
+    this.setState({
+      isMousedown: true,
+    })
+  }
+
+  _onMouseUp() {
+    this.setState({
+      isMousedown: false,
+    })
+  }
+
   static padNumber(number: number) {
     return number < 10 ? `0${number}` : number
   }
@@ -168,9 +209,9 @@ class MoviePlayer extends React.PureComponent<Props, State> {
 
     const minutes = Math.floor(time / 60);
     const hours = Math.floor(time / (60 * 60));
-    const seconds = Math.floor(time % minutes);
+    const seconds = Math.floor(time - +(hours * 60 * 60) - +(minutes * 60));
 
-    return `${hours ? `${MoviePlayer.padNumber(hours)}:` : ''}${minutes ? `${MoviePlayer.padNumber(minutes)}:` : ''}${MoviePlayer.padNumber(seconds)}`;
+    return `${hours ? `${MoviePlayer.padNumber(hours)}` : '00'}:${minutes ? `${MoviePlayer.padNumber(minutes)}` : '00'}:${MoviePlayer.padNumber(seconds)}`;
   }
 }
 
