4297cca866166e7841fe94bcf0581bc9504ec8c7
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-06-22 14:48:45 +0500

    fix no redux error

--- src/index.tsx
+++ src/index.tsx
@@ -16,9 +16,9 @@ const api = createAPI((action: Action) => store.dispatch(action));
 declare const __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (config: {}) => any;
 
 const composeEnhancers =
-  typeof window === `object` &&
-  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+    typeof window === 'object' &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+      (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
 const enhancer = composeEnhancers(applyMiddleware(thunk.withExtraArgument(api)));
 
